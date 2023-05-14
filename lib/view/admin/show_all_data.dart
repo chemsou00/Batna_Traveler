51b19180278a2cdeda3a0b58777e4d835ee7dd41
chemsou00 <souchem00@gmail.com>
2023-05-14 17:43:53 +0100

    add Hotel Card(admin_panel)

--- lib/view/admin/show_all_data.dart
+++ lib/view/admin/show_all_data.dart
@@ -39,9 +39,66 @@ class ShowAllData extends StatelessWidget {
                 elevation: 3,
                 child: InkWell(
                   onTap: () {},
-                  child: const SizedBox(
+                  child: SizedBox(
                     height: 100,
                     width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          ClipRRect(
+                            clipBehavior: Clip.antiAlias,
+                            borderRadius: BorderRadius.circular(20),
+                            child: SizedBox(
+                              width: 80,
+                              //
+                              child: Image.asset("assets/images/logo2.png"),
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            width: Get.size.width * 0.5,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text("Name : Jawhara"),
+                                Text(
+                                  "Address : Alger Centre 1er Nov ",
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                Text("Phone : 033556633"),
+                                Text("Rooms : 100"),
+                                Text("Id : 23"),
+                              ],
+                            ),
+                          ),
+                          const VerticalDivider(
+                            color: Colors.black,
+                            width: 2,
+                          ),
+                          Flexible(
+                              child: Container(
+                                alignment: Alignment.center,
+                                  // color: Colors.blue,
+                                padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text(
+                                  "Rate",
+                                  style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
+                                ),
+                                const Text(
+                                  "4.2",
+                                  style: TextStyle(fontSize: 25),
+                                ),
+                                Icon(Icons.star,color: Colors.yellow[600],)
+                              ],
+                            ),
+                          ))
+                        ],
+                      ),
+                    ),
                   ),
                 ),
               ),
